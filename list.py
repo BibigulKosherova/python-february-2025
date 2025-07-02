fdaa747f3c86246adcc410b42c23a4288a6eb5fb
BibigulKosherova <bibigulkosherova@gmail.com>
2025-07-02 21:33:44 +0000

    added new files

---
+++ list.py
@@ -0,0 +1,14 @@
+food=["burger", "pizza", "taco"]
+drink=["coke", "pepsi", "juice"]
+
+food.append("sushi")
+food.extend(["cheeseburger", "burrito"])
+print(food)
+food.remove("burrito")
+print(food)
+# print(drink)
+# print(food[0])
+# print(drink[2])
+# print(food[-1])
+# print(drink[-2])
+# print(f"Can I have {food[0]} and {drink[1]}?")
---
+++ tuple.py
@@ -0,0 +1,16 @@
+food=("burger", "pizza", "taco")
+drink=("coke", "pepsi", "juice")
+# print(food)
+# print(drink)
+# print(food[0])
+# print(drink[2])
+# print(food[-1])
+# print(drink[-2])
+
+# print(f"Can I have {food[0]} and {drink[1]}?")
+
+
+my_list=list(food)
+my_list.append("sushi")
+# food.extend(["cheeseburger", "burrito"])
+print(my_list)
