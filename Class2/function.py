df007de4a7fde10123370d9bdb3e3a28b29b6130
BibigulKosherova <bibigulkosherova@gmail.com>
2025-07-07 20:09:53 +0000

    added class2 files

---
+++ Class2/function.py
@@ -0,0 +1,8 @@
+def hello(a, b, c, d):
+    print("Hello", a)
+    print("Hello", b)
+    print("Hello", c)
+    print("Hello", d)
+
+
+hello("Tim", "Ann", "Brad", "Ty")
---
+++ Class2/task3.py
@@ -0,0 +1,12 @@
+# sum=0
+# for i in range(1, 101):
+#     sum +=i
+# print(sum)
+
+
+# for i in range(100, 0, -1):
+#     print(i)
+
+number=int(input("Enter the number: "))
+for i in range(0,11):
+    print(number*i)
